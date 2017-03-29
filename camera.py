d0a0722d2fa19942ed70582a14070f2c04ab0495
Tomasz Kasprzyk <kasptom@gmail.com>
2017-03-29 18:42:50 +0200

    Cleanup code

--- camera.py
+++ camera.py
@@ -9,13 +9,30 @@ import zbar
 import cv2
 from PIL import Image
 
-# create a reader
+# create and configure a reader
 scanner = zbar.ImageScanner()
-
-# configure the reader
 scanner.parse_config('enable')
 
 cap = cv2.VideoCapture()
+
+
+def mark_qr_code(vertices):
+    cv2.line(frame, vertices[0], vertices[1], (0, 0, 255), 3)
+    cv2.line(frame, vertices[1], vertices[2], (0, 255, 0), 3)
+    cv2.line(frame, vertices[2], vertices[3], (255, 0, 0), 3)
+    cv2.line(frame, vertices[3], vertices[0], (0, 255, 255), 3)
+
+
+def print_qr_info(symbol):
+    print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+    print 'distance' '%s', calculate_distance(symbol.location)
+    for vertex in symbol.location:
+        print 'point', '(%s, %s)' % vertex
+
+
+def calculate_distance(vertices):
+    pass
+
 
 while True:
     if not cap.isOpened():
@@ -27,13 +44,6 @@ while True:
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    ###########################
-    # create a reader
-    scanner = zbar.ImageScanner()
-
-    # configure the reader
-    scanner.parse_config('enable')
-
     # obtain image data
     pil = Image.fromarray(gray)
     width, height = pil.size
@@ -42,36 +52,21 @@ while True:
     # wrap image data
     image = zbar.Image(width, height, 'Y800', raw)
 
-    # scan the image for barcode(s)
+    # scan the image for qr codes
     scanner.scan(image)
 
     # extract results
-    for symbol in image:
-        # do something useful with results
-        print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
-        vertices = symbol.location
-        # draw lines on the gray frame
-        cv2.line(frame, vertices[0], vertices[1], (0, 0, 255))
-        cv2.line(frame, vertices[1], vertices[2], (0, 255, 0))
-        cv2.line(frame, vertices[2], vertices[3], (255, 0, 0))
-        cv2.line(frame, vertices[3], vertices[0], (0, 255, 255))
-        for vertex in vertices:
-            print 'point', '(%s, %s)' % vertex
-
-    ###########################
+    for qr_code in image:
+        mark_qr_code(qr_code.location)
+        print_qr_info(qr_code)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    # clean up
     del image
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-###
-raw = gray
-width = gray.columns
-height = gray.rows
